898dbb743922fe29deac19a3a165cd832dc52960
kowalskifabio-cpu <kowalski.fabio@gmail.com>
2025-12-10 15:37:56 -0300

    atualiza

--- data/produtos.js
+++ data/produtos.js
@@ -1,15 +1,5 @@
 export const produtos = [
-  {
-    id: 1,
-    nome: "Notebook Gamer Acer Nitro 5 - O melhor Custo Benefício",
-    img: "https://http2.mlstatic.com/D_NQ_NP_963627-MLA51368686616_092022-O.webp", // Exemplo real
-    precoAntigo: 4500.00,
-    precoAtual: 3899.00,
-    parcelamento: "8x sem juros",
-    linkAfiliado: "https://www.mercadolivre.com.br", // Você trocará pelo seu link
-    categoria: "Computadores",
-    destaque: true
-  },
+  ,
  , {
     id: 1765387389765,
     nome: "Samsung Galaxy S24 Plus (eSIM) 5G Dual SIM 256 GB creme 12 GB RAM",
