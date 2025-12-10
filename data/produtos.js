6b597b5d907630282da4d27208454f8cf02ed193
kowalskifabio-cpu <kowalski.fabio@gmail.com>
2025-12-10 15:01:30 -0300

    ajuste

--- data/produtos.js
+++ data/produtos.js
@@ -10,28 +10,7 @@ export const produtos = [
     categoria: "Computadores",
     destaque: true
   },
-  {
-    id: 2,
-    nome: "Fone Bluetooth TWS - Grave Potente",
-    img: "https://http2.mlstatic.com/D_NQ_NP_603837-MLA46663677335_072021-O.webp",
-    precoAntigo: 120.00,
-    precoAtual: 59.90,
-    parcelamento: "3x sem juros",
-    linkAfiliado: "https://www.mercadolivre.com.br",
-    categoria: "Áudio",
-    destaque: false
-  },
-  {
-    id: 3,
-    nome: "Mouse Logitech G203 Lightsync RGB",
-    img: "https://http2.mlstatic.com/D_NQ_NP_666797-MLA46223597475_052021-O.webp",
-    precoAntigo: 199.00,
-    precoAtual: 149.90,
-    parcelamento: "Sem juros",
-    linkAfiliado: "https://www.mercadolivre.com.br",
-    categoria: "Periféricos",
-    destaque: false
-  } , {
+ , {
     id: 1765387389765,
     nome: "Samsung Galaxy S24 Plus (eSIM) 5G Dual SIM 256 GB creme 12 GB RAM",
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_832160-MLA99437063692_112025-F.webp",
