01802b6da301cbfe73b9cbe544ea536ac7767668
kowalskifabio-cpu <kowalski.fabio@gmail.com>
2025-12-10 15:39:46 -0300

    1

--- data/produtos.js
+++ data/produtos.js
@@ -1,6 +1,6 @@
 export const produtos = [
-  ,
- , {
+  
+  {
     id: 1765387389765,
     nome: "Samsung Galaxy S24 Plus (eSIM) 5G Dual SIM 256 GB creme 12 GB RAM",
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_832160-MLA99437063692_112025-F.webp",
