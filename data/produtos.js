f48b9625e6daaf32b00ad883c6d9a63d98a29018
kowalskifabio-cpu <kowalski.fabio@gmail.com>
2025-12-21 20:06:11 -0300

    notebook

--- data/produtos.js
+++ data/produtos.js
@@ -1,6 +1,36 @@
 export const produtos = [
   
   {
+    id: 1766358323415,
+    nome: "Notebook Asus Vivobook 15 X1504va Intel Core I5 1334u 8gb Ram 512gb Ssd Intel Iris Xe Windows 11 Home Tela 15,6 Fhd Silver - Nj1740w",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_674676-MLA83523568962_042025-F.webp",
+    precoAntigo: 4000.00,
+    precoAtual: 2785.20,
+    parcelamento: "10x sem juros",
+    linkAfiliado: "https://mercadolivre.com/sec/2t4niy2",
+    categoria: "Computadores",
+    destaque: true
+  },{
+    id: 1766358159215,
+    nome: "Notebook Acer Vero AV15-53P-56HM Intel® Core™ i5 – 1335U 15.6” Full HD Intel® Iris® Xe 512 GB SSD 8 GB RAM Windows 11 Home",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_894231-MLU78002335839_072024-F.webp",
+    precoAntigo: 4440.28,
+    precoAtual: 2999.90,
+    parcelamento: "10x sem juros",
+    linkAfiliado: "https://mercadolivre.com/sec/2fsYnuj",
+    categoria: "Computadores",
+    destaque: true
+  },{
+    id: 1766357989984,
+    nome: "Notebook Samsung Galaxy Book4 Intel® Core™ i5-1335U (1.3 Ghz, até 4.6GHz, 12 MB L3 Cache), Windows 11 Home, 8GB, 512GB SSD, Iris Xe, 15.6'' Full HD LED, 1.55kg",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_607341-MLU76379088306_052024-F.webp",
+    precoAntigo: 3799.00,
+    precoAtual: 3390.55,
+    parcelamento: "10x sem juros",
+    linkAfiliado: "https://mercadolivre.com/sec/2rWSD5V",
+    categoria: "Computadores",
+    destaque: true
+  },{
     id: 1765820394274,
     nome: "Notebook Lenovo Ideapad 1i Intel Core i5 - 1235U, 8GB RAM, 512GB SSD, Windows 11 Home, Tela 15,6'' HD - 82VY000QBR",
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_615718-MLU73058616092_112023-F.webp",
